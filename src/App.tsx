309bed62445a220dadfcd495b0275eff47a2efb3
unknown <feyibabalola2000@gmail.com>
2024-03-10 04:37:57 +0100

    new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,61 @@
-import { useState } from "react";
-import TodoItem from "./components/TodoItem"
+import { useEffect, useState } from "react";
 import { dummyData } from "./data/todos"
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import TodoSummary from "./components/TodoSummary";
+import { Todo } from "./types/todos";
 
 function App() {
-  const [todos, setTodos] = useState(dummyData);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos: Todo[] = JSON.parse(
+      localStorage.getItem("todos") || "[]"
+    );
+    return savedTodos.length > 0 ? savedTodos : dummyData
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
   
   function setTodoCompleted(id: number, completed: boolean){
     setTodos((prevTodos) => 
      prevTodos.map((todo) => (todo.id === id ? {...todo, completed} : todo ))
     );
 }
+
+function addTodo(title: string){
+  setTodos((prevTodos) => [
+    {
+      id: Date.now(),
+      title,
+      completed: false
+    },
+    ...prevTodos
+  ]);
+}
+
+function deleteTodo(id: number){
+  setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+}
+
+function deleteAllCompletedTodos(){
+  setTodos(prevTodos => prevTodos.filter((todo) => !todo.completed));
+}
   return (
-   <main className=" py-10 h-screen space-y-5">
+   <main className=" py-10 h-screen space-y-5 overflow-y-auto">
     <h1 className=" text-3xl font-extrabold text-center">
       Your todos
     </h1>
-    <div className=" max-w-lg mx-auto bg-slate-100 rounded-md p-5">
-      <AddTodoForm/>
-      <div className="space-y-2">
-        {todos.map(todo => (
-          <TodoItem 
-          key={todo.id}
-          todo={todo}
-          onCompletedChange={setTodoCompleted}
-          />
-        ))}
+    <div className=" max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+      <AddTodoForm onSubmit={addTodo}/>
+      <TodoList todos={todos} onCompletedChange={setTodoCompleted}
+      onDelete={deleteTodo}
+      />
       </div> 
-    </div>
+      <TodoSummary
+      todos={todos}
+      deleteAllCompleted={deleteAllCompletedTodos}
+      />
    </main>
   )
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,14 +1,16 @@
+import { Trash2 } from "lucide-react";
 import { Todo } from "../types/todos"
 
 interface TodoItemProps{
     todo: Todo
     onCompletedChange: (id: number, completed: boolean) => void;
+    onDelete: (id: number) => void;
 }
 
-export default function TodoItem({todo, onCompletedChange}: TodoItemProps){
+export default function TodoItem({todo, onCompletedChange, onDelete}: TodoItemProps){
     return(
-        <div>
-            <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50 cursor-pointer ">
+        <div className="flex items-center gap-1">
+            <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50 cursor-pointer grow ">
                 <input type="checkbox" 
                 checked={todo.completed}
                 onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
@@ -18,6 +20,11 @@ export default function TodoItem({todo, onCompletedChange}: TodoItemProps){
                     {todo.title}
                 </span>
             </label>
+            <button
+            onClick={() => onDelete(todo.id)}
+             className="p-2">
+                <Trash2 size={20} className=" text-gray-500 "/>
+            </button>
         </div>
     )
 }
